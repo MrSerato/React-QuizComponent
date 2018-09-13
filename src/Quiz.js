5868cf717f4857c35af27d6fb2e6d45f51d786ee
Anthony Clementi <atwon13@gmail.com>
2018-09-13 13:40:05 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
     this.state = {quiz_position: 1}
   }
 
-  return() {
+  render() {
     return(
       <div><div className="Quiz Question">
       {quizData.quiz_questions[0].instruction_text}
